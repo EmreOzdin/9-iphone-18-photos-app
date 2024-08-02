4e8f74e4420362483f8f52fdb27c277c58745dd1
Emre Özdin <mrzdnboun@gmail.com>
2024-08-02 17:16:47 +0300

    "Replaced FlatList with ScrollView, added header section with FlatList, and added three new sections with text and gray rectangles."

--- App.tsx
+++ App.tsx
@@ -1,25 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, FlatList } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  FlatList,
+  ScrollView,
+} from "react-native";
 import { photos } from "./data";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <FlatList
-        data={photos}
-        numColumns={5}
-        contentContainerStyle={{ gap: 2 }}
-        columnWrapperStyle={{ gap: 2 }}
-        renderItem={({ item }) => (
-          <Image
-            source={item.image}
-            style={{ width: `${100 / 4}%`, aspectRatio: 1 }}
-          />
-        )}
-      />
+    <ScrollView style={styles.container}>
+      {/* Header */}
+      <View style={{ height: 400 }}>
+        <FlatList
+          data={photos}
+          numColumns={5}
+          contentContainerStyle={{ gap: 2 }}
+          columnWrapperStyle={{ gap: 2 }}
+          scrollEnabled={false}
+          renderItem={({ item }) => (
+            <Image
+              source={item.image}
+              style={{ width: `${100 / 4}%`, aspectRatio: 1 }}
+            />
+          )}
+        />
+      </View>
+      <View>
+        <Text>Albums</Text>
+        <View
+          style={{ height: 200, width: 300, backgroundColor: "lightgray" }}
+        ></View>
+      </View>
+
+      <View>
+        <Text>Albums</Text>
+        <View
+          style={{ height: 200, width: 300, backgroundColor: "lightgray" }}
+        ></View>
+      </View>
+
+      <View>
+        <Text>Albums</Text>
+        <View
+          style={{ height: 200, width: 300, backgroundColor: "lightgray" }}
+        ></View>
+      </View>
 
       <StatusBar style="auto" />
-    </View>
+    </ScrollView>
   );
 }
 
