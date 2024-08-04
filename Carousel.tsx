2a1e320461c7ba59d5c4a3477e815bb0ca81b9ab
Emre Özdin <mrzdnboun@gmail.com>
2024-08-04 19:24:20 +0300

    "Added StyleSheet import and created styles for Carousel component"

--- Carousel.tsx
+++ Carousel.tsx
@@ -1,10 +1,11 @@
-import { ScrollView, Text, View, Image } from "react-native";
+import { ScrollView, Text, View, Image, StyleSheet } from "react-native";
 import { photos } from "./data";
 
 export default function Carousel() {
   return (
     <View>
       <Text>Title</Text>
+
       <ScrollView>
         <Image source={photos[0].image} style={{ width: 300, height: 200 }} />
         <Image source={photos[0].image} style={{ width: 300, height: 200 }} />
@@ -14,4 +15,24 @@ export default function Carousel() {
   );
 }
 
-//https://www.youtube.com/watch?v=pZHRFNpYkAY 35:52
+const styles = StyleSheet.create({
+  container: {
+    marginVertical: 10,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "700",
+    padding: 15,
+  },
+  image: {
+    width: 250,
+    height: 150,
+    paddingHorizontal: 20,
+  },
+  images: {
+    paddingHorizontal: 20,
+    gap: 15,
+  },
+});
+
+//https://www.youtube.com/watch?v=pZHRFNpYkAY 40:52
